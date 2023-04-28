1033e808ee7ce49190bf0f38d43557c7fa6255de
Greg Rell <rell.greg@gmail.com>
2023-04-27 22:08:28 -0400

    Styling

--- vanapp/src/Pages/Home.jsx
+++ vanapp/src/Pages/Home.jsx
@@ -6,15 +6,24 @@ export default function Home(){
     return(
         <div>
             <nav className='navbar'>
-                <h1>#VANLIFE</h1>
-                <Link to="/about">About</Link>
-                <Link to="/">Home</Link>            
+                <p className='navbar--title'>#VANLIFE</p>
+                <Link to="/about" className='navbar--link'>About</Link>
+                <Link to="/" className='navbar--link'>Vans</Link>            
             </nav>
 
             <div className='homepage'>
                 <img className='homepage--image' src={backgroundimage}/>
-
+                <p className='homepage--text'>You got the travel plans, we got the travel vans.</p>
+                <p className='homepage--subtext'>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+                <button className='homepage--button'>Find your van</button>
             </div>
+
+            <div className='footer'>
+                <p className='footer--text'>Ⓒ 2022 #VANLIFE</p>
+            </div>
+
+
+
         </div>
     )
 }
