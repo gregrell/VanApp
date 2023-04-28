1c0287476b2bce14d04d49163b17e6cd0014b992
Greg Rell <rell.greg@gmail.com>
2023-04-27 23:08:23 -0400

    Styling complete on home and about pages

--- vanapp/src/Pages/About.jsx
+++ vanapp/src/Pages/About.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import backgroundimage from '.././Images/image 54.png'
+
 
 
 export default function About(){
     return(
         <div>
-            <h1>About Page</h1>
+             <nav className='navbar'>
+                <p className='navbar--title'>#VANLIFE</p>
+                <Link to="/about" className='navbar--link'>About</Link>
+                <Link to="/" className='navbar--link'>Vans</Link>            
+            </nav>
+
+
+            <div className='about'>
+            <img src={backgroundimage}/>
+                <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+                <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
+                (Hitch costs extra 😉)</p>
+                <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+
+                <div className='about--square'>
+                    <h2 className="about--square--text">Your destination is waiting. Your van is ready.</h2>
+                    <button className='about--square--button'>Explore our vans</button>
+
+                </div>                
+            </div>
+
+            <div className='footer'>
+                 <p className='footer--text'>Ⓒ 2022 #VANLIFE</p>
+            </div>
+
+           
+
+
+
+
         </div>
     )
 }
