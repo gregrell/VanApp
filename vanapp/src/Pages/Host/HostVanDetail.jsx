aa3fba5ca972e13287e12245bfafc44d04dd2e69
Greg Rell <rell.greg@gmail.com>
2023-05-16 01:05:52 -0400

    update

--- vanapp/src/Pages/Host/HostVanDetail.jsx
+++ vanapp/src/Pages/Host/HostVanDetail.jsx
@@ -38,28 +38,6 @@ export default function HostVanDetail(){
     },[])
 
 
-//     description
-// : 
-// "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!"
-// hostId
-// : 
-// "123"
-// id
-// : 
-// "1"
-// imageUrl
-// : 
-// "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png"
-// name
-// : 
-// "Modest Explorer"
-// price
-// : 
-// 60
-// type
-// : 
-// "simple"
-
 
     return(
         <>
--- vanapp/src/Pages/Host/VanDetails/HostVanDetailDetails.jsx
+++ vanapp/src/Pages/Host/VanDetails/HostVanDetailDetails.jsx
@@ -5,6 +5,28 @@ export default function HostVanDetailDetails(props){
 
     const currentVan  = useOutletContext()
 
+//     description
+// : 
+// "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!"
+// hostId
+// : 
+// "123"
+// id
+// : 
+// "1"
+// imageUrl
+// : 
+// "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png"
+// name
+// : 
+// "Modest Explorer"
+// price
+// : 
+// 60
+// type
+// : 
+// "simple"
+
     return(
         <>
             <p>Host Van Detail  {currentVan.name} </p>
