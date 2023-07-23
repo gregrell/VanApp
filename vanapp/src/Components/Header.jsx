748cb1fbd28df597c283dae81c78ab37915a2d32
Greg Rell <rell.greg@gmail.com>
2023-07-23 16:24:41 -0400

    Created Login Form

--- vanapp/src/Components/Header.jsx
+++ vanapp/src/Components/Header.jsx
@@ -13,7 +13,7 @@ export default function Nav(){
             <NavLink to="/host" className={({isActive})=> isActive ? "navbar--link--active" : "navbar--link"}>Host</NavLink>
             <NavLink to="/about" className={({isActive})=> isActive ? "navbar--link--active" : "navbar--link"}>About</NavLink>
             <NavLink to="/vans" className={({isActive})=> isActive ? "navbar--link--active" : "navbar--link"}>Vans</NavLink>          
-            <NavLink to="/login" className={({isActive})=> isActive ? "navbar--link--active" : "navbar--link"}><LoginIcon height='1.3em' width=''/></NavLink>          
+            <NavLink to="/login" className={({isActive})=> isActive ? "navbar--link--active" : "navbar--link"}><LoginIcon height='1.3em' width='1.3em'/></NavLink>          
         </nav>
 
     )
--- vanapp/src/Pages/Login.jsx
+++ vanapp/src/Pages/Login.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { useState } from "react";
+
 
 export default function Login(){
+    const [formState, setFormState] = useState({username:"",password:""})
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(e)
+    }
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setFormState(old=>({
+                ...old,
+                [name]:value  
+        }))
+    }
+
+
+
+
+
+
+
     return(
         <>
-            <h1>Login</h1>
+            <h1>Sign in to Your Account</h1>
+            <form onSubmit={handleSubmit} className="form">
+                <input  className="form--field form--field--top"    
+                        type='text'
+                        name='username'
+                        placeholder="email"
+                        onChange={handleChange}></input>
+                <input  className="form--field form--field--bottom"
+                        type='password'
+                        name='password'
+                        placeholder="password"
+                        onChange={handleChange}></input>
+
+                <button type="submit"
+                        className='form--button'>Sign In</button>
+
+            </form>
         </>
     )
 }
