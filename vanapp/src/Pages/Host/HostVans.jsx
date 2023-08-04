3a5c49ce7a87398758fa94709bb2bc2c9f27e9ee
Greg Rell <rell.greg@gmail.com>
2023-08-03 21:35:24 -0400

    Host vans list changed to use defer/await/suspense

--- vanapp/src/Pages/Host/HostVans.jsx
+++ vanapp/src/Pages/Host/HostVans.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import {React, Suspense} from "react";
+import { useLoaderData, defer, Await } from "react-router-dom";
 import HostVan from "../../Components/HostVan";
 import { getHostVans } from "../../api";
 import { requireAuth } from "../../utils";
@@ -8,7 +8,7 @@ import { requireAuth } from "../../utils";
 
 export async function HostVansloader({request}){
     await requireAuth(request)
-    return getHostVans()
+    return  defer({hostVans: getHostVans()})
 }
 
 
@@ -16,33 +16,32 @@ export async function HostVansloader({request}){
 export default function NavVans(){
 
    
-    const vansdata = useLoaderData()
+    const hostVansDataPromise = useLoaderData()
 
 
 
-    // server.create("van", 
-    // { id: "6", 
-    // name: "Green Wonder", 
-    // price: 70, 
-    // description: "With this van, you can take your travel life to the next level. 
-    // The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.", 
-    // imageUrl: "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png", 
-    // type: "rugged", 
-    // hostId: "123" })
-
-
-    const vansList = vansdata.map((instancedata, index)=>{
+    function renderVanElements(vansdata){
+        const vansList = vansdata.map((instancedata, index)=>{
+            return(
+                <HostVan
+                    key={instancedata.id}
+                    id={instancedata.id}
+                    imageUrl={instancedata.imageUrl}
+                    name={instancedata.name}
+                    price={instancedata.price}
+                    /> 
+            )
+        })
+        
+        
         return(
-            <HostVan
-                key={instancedata.id}
-                id={instancedata.id}
-                imageUrl={instancedata.imageUrl}
-                name={instancedata.name}
-                price={instancedata.price}
-                />                    
-
+            <>
+                {vansList}
+            </>
         )
-    })
+    }
+
+    
 
 
 
@@ -50,7 +49,15 @@ export default function NavVans(){
         <>  
             <div>
                 <h1>Your Listed Vans</h1>
-                {vansList}
+                <Suspense fallback={<>Loading</>}>
+                    <Await
+                        resolve={hostVansDataPromise.hostVans}>
+                        
+                        {renderVanElements}
+
+
+                    </Await>
+                </Suspense>
             </div>
         </>
     )
